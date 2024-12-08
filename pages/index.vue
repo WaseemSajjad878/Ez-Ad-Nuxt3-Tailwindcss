7bba390635b4995a12274e67e1501c1d6d53dab1
Waseem <wase804@gmail.com>
2024-12-08 12:07:37 +0500

    metas added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,47 @@
 
 <script setup>
+useHead({
+  title: "Best Digital Signage for Gyms & Fitness Centers | Ezad TV",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Keep members informed and inspired during workouts with our vibrant digital signage for gym. Elevate the fitness experience today!",
+    },
+    { name: "keywords", content: "vue, nuxt, seo, tutorial" },
+    {
+      content: "Best Digital Signage for Gyms & Fitness Centers | Ezad TV",
+      property: "og:title",
+    },
+    {
+      content:
+        "Keep members informed and inspired during workouts with our vibrant digital signage for gym. Elevate the fitness experience today!",
+      property: "og:description",
+    },
+    {
+      content: "Best Digital Signage for Gyms & Fitness Centers | Ezad TV",
+      property: "twitter:title",
+    },
+    {
+      content:
+        "Keep members informed and inspired during workouts with our vibrant digital signage for gym. Elevate the fitness experience today!",
+      property: "twitter:description",
+    },
+    {
+      content:
+        "https://storage.googleapis.com/content.ezadtv.com/2024/12/06/optimized_6753566385ffe_ez-ad-tv-meta-banner.webp",
+      property: "og:image",
+    },
+    {
+      content:
+        "https://storage.googleapis.com/content.ezadtv.com/2024/12/06/optimized_6753566385ffe_ez-ad-tv-meta-banner.webp",
+      property: "twitter:image",
+    },
+  ],
+  link: [
+    { rel: "canonical", href: "https://nuxt3-landing-page-three.vercel.app/" },
+  ],
+});
 </script>
 <template>
   <main>
